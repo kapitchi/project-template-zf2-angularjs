1bbfcb32969e0866ae3d8690a88d753bfa99ea2e
Matus Zeman <matus.zeman@gmail.com>
2014-08-04 21:48:13 +0200

    backup

---
+++ module/KapAuthentication/src/KapAuthentication/Authentication/Adapter/AdapterManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zemi
+ * Date: 11/05/14
+ * Time: 15:18
+ */
+
+namespace KapAuthentication\Authentication\Adapter;
+
+
+class AdapterManager {
+
+} 
---
+++ module/KapAuthentication/src/KapAuthentication/Controller/CallbackController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zemi
+ * Date: 11/05/14
+ * Time: 17:31
+ */
+
+namespace KapAuthentication\Controller;
+
+
+class OauthController {
+
+} 
