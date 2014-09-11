39a3fc062449cc18987b856b9c8a9645f926da7f
Matus Zeman <matus.zeman@gmail.com>
2014-09-11 19:25:20 +0200

    layout changes kap-ng

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,6 +1,17 @@
 <?php
 return array(
     'router' => array(
-        'routes' => array(),
-    )
+        'routes' => array(
+            'oauth' => array(
+                'options' => array(
+                    'route' => '/oauth',
+                ),
+            ),
+        ),
+    ),
+    'zf-oauth2' => array(
+        'options' => array(
+            'always_issue_new_refresh_token' => true,
+        ),
+    ),
 );
